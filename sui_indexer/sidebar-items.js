c441c08c0a4d4dcb849dd6ab35cdc2b472009dc4
foukstr <foukstr@users.noreply.github.com>
2024-04-21 04:21:12 +0000

    deploy: 48cf4744e6693bd2fc55dda15464c2d6689626ca

---
+++ sui_indexer/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["build_json_rpc_server"],"macro":["chunk"],"mod":["apis","db","errors","framework","handlers","indexer","indexer_reader","metrics","models","schema","store","system_package_task","test_utils","types"],"struct":["IndexerConfig"]};
